159880e2dc94d668f15d3d6a0d456a738197ffce
salman-tyagi <salmantyagi15@gmail.com>
2026-04-05 11:54:49 +0530

    added pagecache key

--- src/services/queries/page-cache.ts
+++ src/services/queries/page-cache.ts
@@ -1,3 +1,4 @@
+import { pageCacheKey } from '$services/keys';
 import { client } from '$services/redis';
 
 const CACHE_ROUTES = ['/about', '/privacy-policy', '/auth/signup', '/auth/signin'];
@@ -5,11 +6,11 @@ const CACHE_ROUTES = ['/about', '/privacy-policy', '/auth/signup', '/auth/signin
 export const getCachedPage = (route: string) => {
 	if (!CACHE_ROUTES.includes(route)) return null;
 
-	return client.get('cachePage#' + route);
+	return client.get(pageCacheKey(route));
 };
 
 export const setCachedPage = (route: string, page: string) => {
 	if (!CACHE_ROUTES.includes(route)) return null;
 
-	return client.set('cachePage#' + route, page, '2s');
+	return client.set(pageCacheKey(route), page, '2s');
 };
