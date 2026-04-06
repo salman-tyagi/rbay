36a34a2702c56ee4f223de1a1edd2f495e940864
salman-tyagi <salmantyagi15@gmail.com>
2026-04-06 22:06:36 +0530

    create user implementation

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,5 +1,22 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+	await client.hset('car', {
+		color: 'red',
+		year: 1950
+		// engine: { cylinders: 8 },
+		// owner: null,
+		// service: undefined
+	});
+
+	const car = await client.hgetall('car');
+
+	if (!car || !Object.keys(car).length) {
+		console.log('Car not found with status code 404');
+		return;
+	}
+
+	console.log(car);
+};
 run();
--- src/services/redis/client.ts
+++ src/services/redis/client.ts
@@ -1,9 +1,7 @@
 import Redis from 'ioredis';
 
-const client = new Redis({
+export const client = new Redis({
 	host: process.env.REDIS_HOST,
 	port: +process.env.REDIS_PORT,
 	password: process.env.REDIS_PW
 });
-
-export { client };
