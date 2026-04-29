5e68d34136a4b1a40a40c80acc558fdeb6a7b62d
salman-tyagi <salmantyagi15@gmail.com>
2026-04-29 17:31:09 +0530

    added custom lua script integration with ioredis

--- src/services/redis/client.ts
+++ src/services/redis/client.ts
@@ -1,7 +1,27 @@
 import Redis from 'ioredis';
 
+interface CustomRedis extends Redis {
+	addOneAndStore(key: string, value: number): Promise<any>;
+}
+
 export const client = new Redis({
 	host: process.env.REDIS_HOST,
 	port: +process.env.REDIS_PORT,
 	password: process.env.REDIS_PW
+}) as CustomRedis;
+
+client.defineCommand('addOneAndStore', {
+	numberOfKeys: 1,
+	lua: `
+		local key = KEYS[1]
+		local newValue = 1 + ARGV[1]
+
+		return redis.call('SET', key, newValue)
+	`
+});
+
+client.on('connect', async () => {
+	await client.addOneAndStore('item:count', 5);
+	const result = await client.get('item:count');
+	console.log(result);
 });
